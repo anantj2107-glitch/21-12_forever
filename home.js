7a1704e84dbd78c5fbbc2672f5cdbf43920d400b
anantj2107-glitch <anantj2107@gmail.com>
2026-02-07 22:51:09 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -10,7 +10,7 @@ startBtn.addEventListener("click", () => {
 function unlockDays() {
 
   // 🔥 CHANGE THIS DAILY
-  const unlockedDay = 1; // change to 2 tomorrow, 3 next day...
+  const unlockedDay = 2; // change to 2 tomorrow, 3 next day...
 
   const links = [
     "rose.html",
